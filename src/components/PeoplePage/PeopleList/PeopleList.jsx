5943309e9313471eb05b93cfe204c1a253db8525
stepGT <stepGT@mail.ru>
2022-02-07 03:30:35 +1000

    Styled PeopleList component

--- src/components/PeoplePage/PeopleList/PeopleList.jsx
+++ src/components/PeoplePage/PeopleList/PeopleList.jsx
@@ -1,10 +1,18 @@
+import styles from './PeopleList.module.css';
+
 const PeopleList = ({ people }) => {
   return (
     <ul className={styles.list__container}>
       {people.map(({ id, name, imgSrc }) => (
         <li className={styles.list__item} key={id}>
-          <img alt={name} src={imgSrc} />
-          <p>{name}</p>
+          <a href="#">
+            <img
+              className={styles.list__person_photo}
+              alt={name}
+              src={imgSrc}
+            />
+            <p>{name}</p>
+          </a>
         </li>
       ))}
     </ul>

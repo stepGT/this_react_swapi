755c75105c11186d18bf3cee4b583103e0886d7d
stepGT <stepGT@mail.ru>
2022-02-11 05:56:30 +1000

    Added react-router and react-router-dom. Added container HomePage

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,10 +1,22 @@
 import PeoplePage from '@containers/PeoplePage';
+import HomePage from '@containers/HomePage';
+import { BrowserRouter, Route, NavLink } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className=''>
-      <PeoplePage />
-    </div>
+    <>
+      <BrowserRouter>
+        <NavLink to="/" exact>
+          Home
+        </NavLink>
+        <NavLink to="/people" exact>
+          People
+        </NavLink>
+
+        <Route path="/" exact component={HomePage} />
+        <Route path="/people" exact component={PeoplePage} />
+      </BrowserRouter>
+    </>
   );
 };
 

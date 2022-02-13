6e352dbd4e412f65d8a5052d7b1d95d6ef91121f
stepGT <stepGT@mail.ru>
2022-02-14 03:10:28 +1000

    Replace buttons in PeopleNavigation

--- src/components/PeoplePage/PeopleNavigation/PeopleNavigation.jsx
+++ src/components/PeoplePage/PeopleNavigation/PeopleNavigation.jsx
@@ -1,30 +1,23 @@
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styles from './PeopleNavigation.module.css';
+import UIButton from '@ui/UIButton';
 
 const PeopleNavigation = ({ getResource, prevPage, nextPage, counterPage }) => {
   const handleChangeNext = () => getResource(nextPage);
   const handleChangePrev = () => getResource(prevPage);
 
   return (
-    <div>
-      <Link to={`/people/?page=${counterPage - 1}`} className={styles.link}>
-        <button
+    <div className={styles.container}>
+      <Link to={`/people/?page=${counterPage - 1}`} className={styles.buttons}>
+        <UIButton
+          text="Previous"
           onClick={handleChangePrev}
           disabled={!prevPage}
-          className={styles.buttons}
-        >
-          Previous
-        </button>
+        />
       </Link>
-      <Link to={`/people/?page=${counterPage + 1}`} className={styles.link}>
-        <button
-          onClick={handleChangeNext}
-          disabled={!nextPage}
-          className={styles.buttons}
-        >
-          Next
-        </button>
+      <Link to={`/people/?page=${counterPage + 1}`} className={styles.buttons}>
+        <UIButton text="Next" onClick={handleChangeNext} disabled={!nextPage} />
       </Link>
     </div>
   );

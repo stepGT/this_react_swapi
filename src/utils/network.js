b4b86684bc3e7f27b29716b431fc4b074420e3f6
stepGT <stepGT@mail.ru>
2022-02-19 01:10:53 +1000

    Added components PersonFilms and UILoading. Added utils makeConcurrentRequest/ChangeHTTP

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,3 +1,5 @@
+import { HTTP, HTTPS } from '@constants/api';
+
 export const getApiResource = async (url) => {
   try {
     const response = await fetch(url);
@@ -10,4 +12,23 @@ export const getApiResource = async (url) => {
     console.error('Could not fetch.', error.message);
     return false;
   }
+};
+
+/**
+ * Изменяет URL с HTTP на HTTPS
+ * @param {String} url - url для изменения
+ * @returns {String} - url с HTTPS
+ */
+export const changeHTTP = (url) => {
+  return url ? url.replace(HTTP, HTTPS) : url;
+};
+
+export const makeConcurrentRequest = async (url) => {
+  const res = await Promise.all(
+    url.map(async (res) => {
+      return await fetch(res).then((res) => res.json());
+    })
+  );
+
+  return res;
 };

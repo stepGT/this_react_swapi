bce5dbada91c8bdf2c94fad1515a3507e2b90ec2
stepGT <stepGT@mail.ru>
2022-02-19 01:42:16 +1000

    Added lazy loading PersonFilms

--- src/containers/PersonPage/PersonPage.jsx
+++ src/containers/PersonPage/PersonPage.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, Suspense } from 'react';
 
 import { withErrorAPI } from '@hoc/withErrorAPI';
 
@@ -11,12 +11,19 @@ import { getApiResource } from '@utils/network';
 import { getPeopleImage } from '@services/getPeopleData';
 import { API_PERSON } from '@constants/api';
 
+import UILoading from '@ui/UILoading';
+
 import styles from './PersonPage.module.css';
+
+const PersonFilms = React.lazy(() =>
+  import('@components/PersonPage/PersonFilms')
+);
 
 const PersonPage = ({ match, setErrorAPI }) => {
   const [personInfo, setPersonInfo] = useState(null);
   const [personName, setPersonName] = useState(null);
   const [personPhoto, setPersonPhoto] = useState(null);
+  const [personFilms, setPersonFilms] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -36,6 +43,7 @@ const PersonPage = ({ match, setErrorAPI }) => {
 
         setPersonName(res.name);
         setPersonPhoto(getPeopleImage(id));
+        res.films.length && setPersonFilms(res.films);
         setErrorAPI(false);
       } else {
         setErrorAPI(true);
@@ -54,6 +62,12 @@ const PersonPage = ({ match, setErrorAPI }) => {
           <PersonPhoto personPhoto={personPhoto} personName={personName} />
 
           {personInfo && <PersonInfo personInfo={personInfo} />}
+
+          {personFilms && (
+            <Suspense fallback={<UILoading />}>
+              <PersonFilms personFilms={personFilms} />
+            </Suspense>
+          )}
         </div>
       </div>
     </>

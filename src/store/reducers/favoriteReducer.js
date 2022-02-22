a1afaede563c4197927ddd0d3a49415bc676ca1f
stepGT <stepGT@mail.ru>
2022-02-23 05:14:52 +1000

    Added localStorage

--- src/store/reducers/favoriteReducer.js
+++ src/store/reducers/favoriteReducer.js
@@ -1,10 +1,11 @@
 import { omit } from 'lodash';
+import { getLocalStorage } from '@utils/localStorage';
 import {
   ADD_PERSON_TO_FAVORITE,
   DELETE_PERSON_TO_FAVORITE,
 } from '@store/constants/actionTypes';
 
-const initialState = {};
+const initialState = getLocalStorage('store');
 
 const favoriteReducer = (state = initialState, action) => {
   switch (action.type) {
